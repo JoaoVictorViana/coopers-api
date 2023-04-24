b64a329667c3037be36ab6012712e83d490cf6d4
João Victor Viana <joao.duarte@smplaces.com.br>
2023-04-23 21:55:05 -0300

    feat(config): configuração do banco para env

--- src/database.module.ts
+++ src/database.module.ts
@@ -2,18 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/user/user.entity';
 import { Todo } from './modules/todo/todo.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT ?? 3306),
-      username: 'root',
-      password: 'root',
-      database: 'coopers',
-      entities: [User, Todo],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, Todo],
+        synchronize: true,
+      }),
     }),
   ],
 })
